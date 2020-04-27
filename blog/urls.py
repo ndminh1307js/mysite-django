74f398cedd9cf080cc10f72d2a3b1170f70148b9
ndminh1307js <ndminh1307js@gmail.com>
2020-04-27 22:03:06 +0700

    Add full-text search engine

--- blog/urls.py
+++ blog/urls.py
@@ -13,5 +13,6 @@ urlpatterns = [
          name='post_detail'),
     path('<int:post_id>/share/', views.post_share, name='post_share'),
     path('tag/<slug:tag_slug>/', views.post_list, name='post_list_by_tag'),
-    path('feed/', LatestPostFeed(), name='post_feed')
+    path('feed/', LatestPostFeed(), name='post_feed'),
+    path('search/', views.post_search, name='post_search'),
 ]
--- mysite/settings.py
+++ mysite/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.sites',
+    'django.contrib.postgres',
     'django.contrib.sitemaps',
     'blog.apps.BlogConfig',
     'taggit',
